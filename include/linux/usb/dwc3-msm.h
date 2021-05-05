1bbce585a97ec4fae34e49f341476c486c38bfde
Alexander Koskovich <zvnexus@outlook.com>
2021-05-05 10:54:34 -0700

    drivers: usb: Import OnePlus 9 changes.
    Change-Id: I3bbd6c2f0eb73c7ca80204f6d0bd2719faeeba26

--- include/linux/usb/dwc3-msm.h
+++ include/linux/usb/dwc3-msm.h
@@ -164,4 +164,11 @@ static bool __maybe_unused usb_get_remote_wakeup_status(struct usb_gadget *gadge
 { return false; }
 #endif
 
+enum oplus_dwc3_notify_event {
+	DWC3_ENUM_DONE,
+};
+
+extern void oplus_dwc3_set_notifier(void (*notify)(enum oplus_dwc3_notify_event event));
+extern int oplus_dwc3_notify_event(enum oplus_dwc3_notify_event event);
+
 #endif /* __LINUX_USB_DWC3_MSM_H */
