09c47e3c5ae88561197caf7f5e1253fcc4af06b5
Vishalcj17 <vishalcj@aospa.co>
2021-06-07 14:56:14 +0900

    drivers: leds: Don't try to get battery resistance data.
     * Conflicts with OnePlus's charger implementation.
    
     * Instead, mark it as 0.
    
    Change-Id: I997298fc9770b9f484ddcad0b5bccce4280e5bf5

--- drivers/leds/leds-qti-flash.c
+++ drivers/leds/leds-qti-flash.c
@@ -892,6 +892,10 @@ static int qti_flash_led_calc_max_avail_current(
 		vph_flash_uv, vin_flash_uv, p_flash_fw;
 	union power_supply_propval prop = {};
 
+#if IS_ENABLED(CONFIG_OPLUS_CHG)
+	rc = 0;
+	rbatt_uohm = 0;
+#else
 	rc = qti_battery_charger_get_prop("battery", BATTERY_RESISTANCE,
 						&rbatt_uohm);
 	if (rc < 0) {
@@ -899,6 +903,7 @@ static int qti_flash_led_calc_max_avail_current(
 				rc);
 		return rc;
 	}
+#endif
 
 	if (!rbatt_uohm) {
 		*max_current_ma = MAX_FLASH_CURRENT_MA;
