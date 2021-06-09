c26a2b326195b4563d22ff78bf8a01084d1fe01c
Alexander Koskovich <zvnexus@outlook.com>
2021-06-08 21:57:17 -0700

    drivers: net: cnss2: Load correct firmware based on region.
     * This is required for WLAN to work correctly, the
       generic firmware will not do.
    
    Change-Id: I6e1353c37e38fcad9bff22267ec1a9cab9cb1d5e

--- drivers/net/wireless/cnss2/main.h
+++ drivers/net/wireless/cnss2/main.h
@@ -505,6 +505,8 @@ struct cnss_plat_data {
 	u64 feature_list;
 };
 
+void cnss_set_bdf_name(u32 version, u32 ext);
+
 #ifdef CONFIG_ARCH_QCOM
 static inline u64 cnss_get_host_timestamp(struct cnss_plat_data *plat_priv)
 {
